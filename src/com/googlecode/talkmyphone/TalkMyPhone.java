77513e7f44692c04f2aa488aa2e2006ede752b48
"Christophe-Marie Duquesne ext:(%22) <chm.duquesne@gmail.com>
2010-09-09 19:08:22 +0200

    Trying to fix the ring bug

--- src/com/googlecode/talkmyphone/TalkMyPhone.java
+++ src/com/googlecode/talkmyphone/TalkMyPhone.java
@@ -144,7 +144,21 @@ public class TalkMyPhone extends Service {
             instance = this;
             getPrefs();
             initSmsMonitors();
+            Uri alert = Settings.System.DEFAULT_RINGTONE_URI ;
             mMediaPlayer = new MediaPlayer();
+            try {
+                mMediaPlayer.setDataSource(this, alert);
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+                mMediaPlayer.setLooping(true);
+            } catch (IllegalArgumentException e1) {
+                e1.printStackTrace();
+            } catch (SecurityException e1) {
+                e1.printStackTrace();
+            } catch (IllegalStateException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
             try {
                 initConnection();
             } catch (XMPPException e) {
@@ -279,24 +293,16 @@ public class TalkMyPhone extends Service {
     }
 
     private void ring() {
-        Uri alert = Settings.System.DEFAULT_RINGTONE_URI ;
-        try {
-            mMediaPlayer.setDataSource(this, alert);
-            final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
-                        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
-                        mMediaPlayer.setLooping(true);
-                        mMediaPlayer.prepare();
-                        mMediaPlayer.start();
+        final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+            try {
+                mMediaPlayer.prepare();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            mMediaPlayer.start();
         }
     }
 
