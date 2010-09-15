3def05229d77b9f8382093f80fc7f1930044ceb5
"Christophe-Marie Duquesne ext:(%22) <chm.duquesne@gmail.com>
2010-09-15 18:11:20 +0200

    Let the user choose dynamically the right application to handle the given uri

--- src/com/googlecode/talkmyphone/XmppService.java
+++ src/com/googlecode/talkmyphone/XmppService.java
@@ -6,9 +6,6 @@ import java.util.Date;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.List;
-import java.util.Locale;
-
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
@@ -30,8 +27,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.location.Address;
-import android.location.Geocoder;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -352,12 +347,12 @@ public class XmppService extends Service {
     /** gets all matching contacts with their ID */
     private Dictionary<Long, String> getContacts(String name) {
         Dictionary<Long, String> res = new Hashtable<Long, String>();
-        
+
         if (name.compareTo("") != 0)
         {
             ContentResolver resolver = getContentResolver();
             String[] projection = new String[] { People._ID, People.NAME };
-    
+
             Uri contactUri = Uri.withAppendedPath(People.CONTENT_FILTER_URI, Uri.encode(name));
             Cursor c = resolver.query(contactUri, projection, null, null, null);
             for (boolean hasData = c.moveToFirst() ; hasData ; hasData = c.moveToNext()) {
@@ -402,7 +397,7 @@ public class XmppService extends Service {
                 command = commandLine;
                 args = "";
             }
-            
+
             if (command.equals("?")) {
                 send("Available commands:");
                 send("- \"?\": shows this help.");
@@ -412,11 +407,8 @@ public class XmppService extends Service {
                 send("- \"number:contact\": show phone number of a specific contact.");
                 send("- \"where\": sends you google map updates about the location of the phone until you send \"stop\"");
                 send("- \"ring\": rings the phone until you send \"stop\"");
-                send("- \"browse:url\": browse an url");
                 send("- \"copy:text\": copy text to clipboard");
-                send("- \"map:address\": launch Google Map on a location");
-                send("- \"nav:address\": launch Google Navigation on a location (if available)");
-                send("- \"street:address\": launch Google Street View on a location (if available)");
+                send("...and pasting a link lets you choose your activity.");
             }
             else if (command.equals("sms")) {
                 String phoneNumber = args.substring(0, args.indexOf(":"));
@@ -440,14 +432,14 @@ public class XmppService extends Service {
             else if (command.equals("readsms")) {
                 int count = 10;
                 String name = args;
-                
+
                 if (-1 != args.indexOf(":")) {
                     name = args.substring(0, args.indexOf(":"));
                     try {
                         count = Integer.parseInt(args.substring(args.indexOf(":") + 1));
                     } catch (Exception e) {
-                    }    
-                } 
+                    }
+                }
                 readSMS(name, count);
             }
             else if (command.equals("where")) {
@@ -463,17 +455,8 @@ public class XmppService extends Service {
                 send("Ringing phone");
                 ring();
             }
-            else if (command.equals("browse")) {
-                browse(args);
-            }
-            else if (command.equals("map")) {
-                maps(args);
-            }
-            else if (command.equals("nav")) {
-                navigate(args);
-            }
-            else if (command.equals("street")) {
-                streetView(args);
+            else if (command.equals("http")) {
+                open("http:" + args);
             }
             else {
                 send('"'+ commandLine + '"' + ": unknown command. Send \"?\" for getting help");
@@ -493,14 +476,14 @@ public class XmppService extends Service {
             while( e. hasMoreElements() ){
                 Long id = e.nextElement();
                 send(contacts.get(id));
-    
+
                 Uri personUri = ContentUris.withAppendedId(People.CONTENT_URI, id);
                 Uri phonesUri = Uri.withAppendedPath(personUri, People.Phones.CONTENT_DIRECTORY);
                 String[] proj = new String[] {Contacts.Phones.NUMBER, Contacts.Phones.LABEL, Contacts.Phones.TYPE};
                 Cursor c = resolver.query(phonesUri, proj, null, null, null);
-               
+
                 for (boolean hasData = c.moveToFirst() ; hasData ; hasData = c.moveToNext()) {
-                    
+
                     String number = cleanNumber(getString(c,Contacts.Phones.NUMBER));
                     String label = getString(c,Contacts.Phones.LABEL);
                     int type = getLong(c,Contacts.Phones.TYPE).intValue();
@@ -540,7 +523,7 @@ public class XmppService extends Service {
     public void readSMS(String contact, Integer count) {
 
         Dictionary<Long, String> contacts = getContacts(contact);
-        
+
         if (contacts.size() > 0) {
             ContentResolver resolver = getContentResolver();
 
@@ -552,7 +535,7 @@ public class XmppService extends Service {
                     Uri mSmsQueryUri = Uri.parse("content://sms/inbox");
                     String columns[] = new String[] { "person", "body", "date", "status"};
                     Cursor c = resolver.query(mSmsQueryUri, columns, "person = " + id, null, null);
-    
+
                     if (c.getCount() > 0) {
                         send(contacts.get(id));
                         Integer i = 0;
@@ -592,84 +575,11 @@ public class XmppService extends Service {
             send("Clipboard access failed");
         }
     }
-    
-    /** launches the browser on the specified url */
-    private void browse(String url) {
-        try {
-            if(!url.contains("//")) {
-                url = "http://" + url;
-            }
-            launchExternal(url);
-            send("Browsing URL \"" + url + "\".");
-        }
-        catch(Exception ex) {
-            send("URL \"" + url + "\" not supported");
-        }
-    }
 
-    /** launches google maps on the specified url */
-    private void maps(String url) {
-        try {
-            if(!url.startsWith("geo:")) {
-                url = "geo:0,0?q=" + url.replace(" ", "+");
-            }
-            launchExternal(url);
-            send("Map on \"" + url + "\".");
-        }
-        catch(Exception ex)
-        {
-            send("\"" + url + "\" not supported");
-        }
-    }
-
-    /** launches navigate on the specified url */
-    private void navigate(String url) {
-        try
-        {
-            if(!url.startsWith("google.navigation:")) {
-                url = "google.navigation:q=" + url.replace(" ", "+");
-            }
-            launchExternal(url);
-            send("Navigate to \"" + url + "\".");
-        }
-        catch(Exception ex) {
-            send("\"" + url + "\" not supported");
-        }
-    }
-
-    /** launches streetview on the specified url */
-    private void streetView(String url) {
-        try {
-            Geocoder geo = new Geocoder(getBaseContext(), Locale.getDefault());
-            List<Address> addresses = geo.getFromLocationName(url, 10);
-            if (addresses.size() > 1) {
-                send("Specify more details:");
-                for (Address address : addresses) {
-                    StringBuilder addr = new StringBuilder();
-                    for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                        addr.append(address.getAddressLine(i) + "\n");
-                    }
-                    send(addr.toString());
-                }
-            }
-            else if (addresses.size() == 1) {
-                Address address = addresses.get(0);
-                launchExternal("google.streetview:cbll=" + address.getLatitude() + "," + address.getLongitude());
-                StringBuilder addr = new StringBuilder();
-                for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                    addr.append(address.getAddressLine(i) + "\n");
-                }
-                send("Street View on \"" + addr + "\".");
-            }
-        }
-        catch(Exception ex) {
-            send("\"" + url + "\" not supported");
-        }
-    }
-
-    /** launches an activity on the url */
-    private void launchExternal(String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    /** lets the user choose an activity compatible with the url */
+    private void open(String url) {
+        Intent target = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        Intent intent = Intent.createChooser(target, "TalkMyPhone: choose an activity");
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
